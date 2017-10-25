d2426617e9a22162d321cc0bf367438b967c21e6
Kornel Lewicki <kornel.lewicki@kcl.ac.uk>
2017-10-25 21:10:24 +0100

    Added new popup file

--- js/popup.js
+++ js/popup.js
@@ -1,30 +1,93 @@
 
-function test() {
-	swal(
-	  'Oops...',
-	  'You have chosen more than three modules ',
-	  'error'
-	)
+function error() {
+    swal(
+        'Oops...',
+        'You have chosen more than three modules ',
+        'error'
+    )
 }
 
-function testChecked() {
-
-	var semester1 = document.getElementByName('SEM1');
-	var semester2 = document.getElementByName('SEM2');
-	var hasChecked = false;
-	var numberOfChecked = 3;
-
-	for (var i = 0; i < semester1.length; i++) {
-
-		if (semester1[i].checked == numberOfChecked && semester2[i].checked==numberOfChecked) {
-			hasChecked=true;
-			test();
-			break;
-		} else {
-			hasChecked=false;
-			test();
-		}
-
-	}
-
+function delay() {
+    swal({
+        title: 'Auto close alert!',
+        text: 'Saving selection',
+        timer: 2000,
+        onOpen: function () {
+            swal.showLoading()
+        }
+    }).then(
+        function () {},
+        // handling the promise rejection
+        function (dismiss) {
+            if (dismiss === 'timer') {
+                console.log('I was closed by the timer')
+            }
+        }
+    )
 }
+
+function confirmation() {
+    swal({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        type: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, Proceed!',
+        cancelButtonText: 'No, cancel!',
+        confirmButtonClass: 'btn btn-success',
+        cancelButtonClass: 'btn btn-danger',
+        buttonsStyling: false
+    }).then(function () {
+        swal(
+            'Saved!',
+            'Changes Saved!',
+            'success'
+        )
+
+
+        delay();
+        saved();
+        timeoutFunction();
+
+    }, function (dismiss) {
+        // dismiss can be 'cancel', 'overlay',
+        // 'close', and 'timer'
+        if (dismiss === 'cancel') {
+            swal(
+                'Cancelled',
+                'Changes not saved!',
+                'error'
+            )
+        }
+    })
+}
+
+function saved() {
+    swal({
+        position: 'center',
+        type: 'success',
+        title: 'Your selection has been saved',
+        showConfirmButton: false,
+        timer: 5000
+    })
+}
+
+function timeoutFunction() {
+    setTimeout(function() {
+        window.location.replace("index.html");
+    }, 3000);
+}
+
+function errorCheck() {
+    swal(
+        'Oops...',
+        'You have chosen more than two modules ',
+        'error'
+    )
+}
+
+
+
+
